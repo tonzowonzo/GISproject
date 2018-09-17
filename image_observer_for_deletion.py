ced1dcf1a3b549dfee62a8e7c590130c8a4cf6f4
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-09-17 20:15:27 +0200

    allows deletion from list

--- image_observer_for_deletion.py
+++ image_observer_for_deletion.py
@@ -4,14 +4,19 @@ import cv2
 import matplotlib.pyplot as plt
 
 # Path of rgb images
-path = r"C:/Users/Tim/Desktop/GIS/GISproject/EC6"
+path = r"C:/Users/Tim/Desktop/GIS/GISproject/17"
 
 # Lists for images with each type.
 good_list = []
 delete_list = []
 
 for img in os.listdir(path):
+    '''
+    Press 1 to keep the image.
+    Press 2 to delete the image.
+    '''
     if img.endswith("tif"):
+        print(img)
         rgb_img = cv2.imread(path + "/" + img)
         rgb_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2RGB)
         plt.imshow(rgb_img)
@@ -24,5 +29,13 @@ for img in os.listdir(path):
         elif is_image_good == "2":
             delete_list.append(img)
             
+            
+# Delete bad list imagery.
+for file in os.listdir(path):
+    for bad_img in delete_list:
+        split_file = bad_img.split(".")
+        split_file = split_file[0]
+        if split_file in file:
+            os.remove(path + "/" + file)
             
         
