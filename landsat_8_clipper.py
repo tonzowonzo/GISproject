1934f00dc8a3588847dc2b5df6108a461309f6b2
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-09-25 18:16:53 +0200

    for use on new 8 bit images

--- landsat_8_clipper.py
+++ landsat_8_clipper.py
@@ -10,21 +10,25 @@ clip_shape = ["EC1", "EC2", "EC3", "EC4", "EC5", "EC6"]
 band_locations = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
 bands = ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11"]
 
+# xy tolerance
+xy_tolerance = ""
+
 # Define function for clipping the image.
 def clip_image(clip_features, in_features, output_location):
     arcpy.Clip_management(in_features, "#", output_location, clip_features,
                           "0", "ClippingGeometry",
-                          maintain_clipping_extent=True)
+                          maintain_clipping_extent=False
+                          )
     
 for shape in clip_shape:
-    if clip_shape in ["EC1", "EC2", "EC3"]:
+    if shape in ["EC1", "EC2", "EC3"]:
         # Define the path/row that is used.
         path_row = "195026"
     else:
         path_row = "194026"
         
     # Loop over landsat image files.
-    for file in os.listdir(rootdir + "landsat//landsat_8"):
+    for file in os.listdir(rootdir + "landsat//landsat_8_8bit"):
         if file.endswith(".TIF"):
             split_file = file.split(".")
             split_file = split_file[0]
@@ -36,7 +40,7 @@ for shape in clip_shape:
                         continue
                     elif split_file[-1] == band:
                         clip_image(rootdir + shape + ".shp",
-                                   rootdir + "landsat//landsat_8//" + file,
+                                   rootdir + "landsat//landsat_8_8bit//" + file,
                                    rootdir + "landsat_8//" + shape + "//" + i + "//" +
-                                   split_file[3] + ".TIF")
+                                   split_file[3] + ".tif")
             
