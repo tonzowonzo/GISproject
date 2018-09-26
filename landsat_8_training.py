8064a4a00cdc5d95608b0cb0ba7727c233def52d
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-09-26 23:41:22 +0200

    fixed looping issue - multiple looping

--- landsat_8_training.py
+++ landsat_8_training.py
@@ -48,101 +48,100 @@ Where:
     lst: land surface temperature.
 '''
 columns = ["date", "day_of_year", "month", "year", "last_crop", "ca", "b", "g",
-           "r", "nir", "swir1",  "swir2", "pan", "cir", "tirs1", "tirs2", 
+           "r", "nir", "swir1",  "swir2", "pan", "cir", "tirs1", "tirs2", "ndvi",
            "label", "binary_label"]
 train_df = pd.DataFrame(columns=columns)
 test_df = pd.DataFrame(columns=columns)
-
-field_areas = ["EC1", "EC2", "EC3", "EC4", "EC5", "EC6"]
+test_field_areas = ["EC3"]
+field_areas = ["EC1", "EC2", "EC3", "EC4", "EC5", "EC6", "1_1", "2", "3",
+               "4", "5", "6", "8", "9", "11", "13", "15", "17"]
 summer_crops = ["SM, CC-SM", "CC-SB", "SP", "CC-GM"]
 winter_crops = ["WW", "WB", "WR"]
-              
 # Loop for getting all files into the dataframe, labelled.
 for field in field_areas:
     path = "C:/Users/Tim/Desktop/GIS/GISproject/landsat_8/" + field
-    for band in os.listdir(path):
-        for file in os.listdir(path + "/" + band):
-            if file.endswith('.tif'):
-                # The year, month and day time.
-                date = file[:-4]
-                print(file, path)
-                date = pd.to_datetime(date, format="%Y%m%d")
-                # What day of the year is it out of 365/366.
-                day_of_year = datetime.datetime.timetuple(date).tm_yday
-                month = datetime.datetime.date(date).month
-                year = datetime.datetime.date(date).year
-                # Get the label based on csv.
-                label, last_crop = get_label(field, date)
-                # Load the training image.
-                ca = cv2.imread(path +  "\\1\\" + file, 0)
-                b = cv2.imread(path +  "\\2\\" + file, 0)
-                g = cv2.imread(path +  "\\3\\" + file, 0)
-                r = cv2.imread(path +  "\\4\\" + file, 0)
-                nir = cv2.imread(path +  "\\5\\" + file, 0)
-                swir1 = cv2.imread(path +  "\\6\\" + file, 0)
-                swir2 = cv2.imread(path +  "\\7\\" + file, 0)
-                pan = cv2.imread(path +  "\\8\\" + file, 0)
-                cir = cv2.imread(path +  "\\9\\" + file, 0)
-                tirs1 = cv2.imread(path +  "\\10\\" + file, 0)
-                tirs2 = cv2.imread(path +  "\\11\\" + file, 0)
-                                
-                # Reshape the panchromatic image to match others.
-                img_shape = b.shape[:2]
-                pan = cv2.resize(pan, dsize=(img_shape[1], img_shape[0]), 
-                                 interpolation=cv2.INTER_NEAREST)
+    for file in os.listdir(path + "/1/"):
+        if file.endswith('.tif'):
+            # The year, month and day time.
+            date = file[:-4]
+            print(file, path)
+            date = pd.to_datetime(date, format="%Y%m%d")
+            # What day of the year is it out of 365/366.
+            day_of_year = datetime.datetime.timetuple(date).tm_yday
+            month = datetime.datetime.date(date).month
+            year = datetime.datetime.date(date).year
+            # Get the label based on csv.
+            label, last_crop = get_label(field, date)
+            # Load the training image.
+            ca = cv2.imread(path +  "\\1\\" + file, 0)
+            b = cv2.imread(path +  "\\2\\" + file, 0)
+            g = cv2.imread(path +  "\\3\\" + file, 0)
+            r = cv2.imread(path +  "\\4\\" + file, 0)
+            nir = cv2.imread(path +  "\\5\\" + file, 0)
+            swir1 = cv2.imread(path +  "\\6\\" + file, 0)
+            swir2 = cv2.imread(path +  "\\7\\" + file, 0)
+            pan = cv2.imread(path +  "\\8\\" + file, 0)
+            cir = cv2.imread(path +  "\\9\\" + file, 0)
+            tirs1 = cv2.imread(path +  "\\10\\" + file, 0)
+            tirs2 = cv2.imread(path +  "\\11\\" + file, 0)
+                            
+            # Reshape the panchromatic image to match others.
+            img_shape = b.shape[:2]
+            pan = cv2.resize(pan, dsize=(img_shape[1], img_shape[0]), 
+                             interpolation=cv2.INTER_NEAREST)
+            
+            # Labels for early or late planting or crop.
+            if label in summer_crops:
+                summer_or_winter_crop = 'summer'
+            elif label in winter_crops:
+                summer_or_winter_crop = 'winter'
+            else:
+                summer_or_winter_crop = 'other'
                 
-                # Labels for early or late planting or crop.
-                if label in summer_crops:
-                    summer_or_winter_crop = 'summer'
-                elif label in winter_crops:
-                    summer_or_winter_crop = 'winter'
-                else:
-                    summer_or_winter_crop = 'other'
-                    
-                # Display the images for checking.
-                plt.imshow(b)
-                plt.show()
-                plt.imshow(tirs2)
-                plt.show()
-                
-                # Turn the 2D image data into a 1D series.
-                ca = ca.ravel()
-                b = b.ravel()
-                g = g.ravel()
-                r = r.ravel()
-                nir = nir.ravel()
-                swir1 = swir1.ravel()
-                swir2 = swir2.ravel()
-                pan = pan.ravel()
-                cir = cir.ravel()
-                tirs1 = tirs1.ravel()
-                tirs2 = tirs2.ravel()
-                
-                # Calculate NDVI.
-                ndvi = (nir - r) / (nir + r)
-                
-                
-                # Create the secondary dataframe to append to the full dataframe.
-                data = {"date": date, "day_of_year": day_of_year, "month": month,
-                        "year": year, "last_crop": last_crop, "ca": ca, "b": b, 
-                        "g": g, "r": r, "nir": nir, "swir1": swir1,  
-                        "swir2": swir2, "pan": pan, "cir": cir, "tirs1": tirs1, 
-                        "tirs2": tirs2, "label": label, 
-                        "binary_label": summer_or_winter_crop}
-                
-                df_iter = pd.DataFrame(data=data)
-                df_iter = df_iter[columns]
-                # Append secondary dataframe to the full dataframe.
-                # Choose which df to add to.
-                random_number = random.randint(1, 100)
-                if random_number <= 20:
-                    test_df = test_df.append(df_iter)
-                else:
-                    train_df = train_df.append(df_iter)
-    #            if field in test_field_areas:
-    #                test_df = test_df.append(df_iter)
-    #            else:
-    #                train_df = train_df.append(df_iter)
+            # Display the images for checking.
+            plt.imshow(b)
+            plt.show()
+            plt.imshow(tirs2)
+            plt.show()
+            
+            # Turn the 2D image data into a 1D series.
+            ca = ca.ravel()
+            b = b.ravel()
+            g = g.ravel()
+            r = r.ravel()
+            nir = nir.ravel()
+            swir1 = swir1.ravel()
+            swir2 = swir2.ravel()
+            pan = pan.ravel()
+            cir = cir.ravel()
+            tirs1 = tirs1.ravel()
+            tirs2 = tirs2.ravel()
+            
+            # Calculate NDVI.
+            ndvi = (nir - r) / (nir + r)
+            
+            
+            # Create the secondary dataframe to append to the full dataframe.
+            data = {"date": date, "day_of_year": day_of_year, "month": month,
+                    "year": year, "last_crop": last_crop, "ca": ca, "b": b, 
+                    "g": g, "r": r, "nir": nir, "swir1": swir1,  
+                    "swir2": swir2, "pan": pan, "cir": cir, "tirs1": tirs1, 
+                    "tirs2": tirs2, "ndvi": ndvi, "label": label, 
+                    "binary_label": summer_or_winter_crop}
+            
+            df_iter = pd.DataFrame(data=data)
+            df_iter = df_iter[columns]
+            # Append secondary dataframe to the full dataframe.
+            # Choose which df to add to.
+#                random_number = random.randint(1, 100)
+#                if random_number <= 20:
+#                    test_df = test_df.append(df_iter)
+#                else:
+#                    train_df = train_df.append(df_iter)
+            if field in test_field_areas:
+                test_df = test_df.append(df_iter)
+            else:
+                train_df = train_df.append(df_iter)
                         
 # Drop the black pixels in the dataframe.
 train_df = train_df[(train_df.r != 0) & (train_df.g != 0) & (train_df.b != 0)]
@@ -152,37 +151,37 @@ test_df = test_df[(test_df.r != 0) & (test_df.g != 0) & (test_df.b != 0)]
 train_df = train_df[train_df.label != "irrelevant"]
 test_df = test_df[test_df.label != "irrelevant"]
 
-## Downsample WW, SM and Cloud.
-#from sklearn.utils import resample
+# Downsample WW, SM and Cloud.
+from sklearn.utils import resample
 #train_df_cloud = train_df[train_df.label == "Cloud"]
-#train_df_SM = train_df[train_df.label == "SM"]
-#train_df_WW = train_df[train_df.label == "WW"]
+train_df_SM = train_df[train_df.label == "SM"]
+train_df_WW = train_df[train_df.label == "WW"]
 #
 ## Downsample Cloud.
 #train_df_cloud = resample(train_df_cloud,
 #                          replace=False,
 #                          n_samples=3000,
 #                          random_state=42)
-## Downsample SM.
-#train_df_SM = resample(train_df_SM,
-#                          replace=False,
-#                          n_samples=3000,
-#                          random_state=42)
-## Downsample WW.
-#train_df_WW = resample(train_df_WW,
-#                          replace=False,
-#                          n_samples=3000,
-#                          random_state=42)
-#
-## Remove the above from train df.
-#train_df = train_df[train_df.label != "Cloud"]
-#train_df = train_df[train_df.label != "SM"]
-#train_df = train_df[train_df.label != "WW"]
-#
-## Append the resampled dfs to train df.
-#train_df = pd.concat([train_df, train_df_cloud])
-#train_df = pd.concat([train_df, train_df_SM])
-#train_df = pd.concat([train_df, train_df_WW])
+# Downsample SM.
+train_df_SM = resample(train_df_SM,
+                          replace=False,
+                          n_samples=3000,
+                          random_state=42)
+# Downsample WW.
+train_df_WW = resample(train_df_WW,
+                          replace=False,
+                          n_samples=3000,
+                          random_state=42)
+
+# Remove the above from train df.
+train_df = train_df[train_df.label != "Cloud"]
+train_df = train_df[train_df.label != "SM"]
+train_df = train_df[train_df.label != "WW"]
+
+# Append the resampled dfs to train df.
+train_df = pd.concat([train_df, train_df_cloud])
+train_df = pd.concat([train_df, train_df_SM])
+train_df = pd.concat([train_df, train_df_WW])
 
 
 # Import libraries
@@ -277,10 +276,10 @@ joblib.dump(rand_for, "random_forest_2.pkl")
 
 # Random forest for without last crop info (1st classification)
 rand_for_no_last_crop = RandomForestClassifier(bootstrap=True, n_estimators=10, random_state=42)
-X_train_no_last_crop = X_train[["month", "year", "ca", "b", "g",
-           "r", "nir", "swir1",  "swir2", "pan", "cir", "tirs1", "tirs2"]]
-X_test_no_last_crop = X_test[["month", "year", "ca", "b", "g",
-           "r", "nir", "swir1",  "swir2", "pan", "cir", "tirs1", "tirs2"]]
+X_train_no_last_crop = X_train[["month", "b", "g",
+           "r", "nir", "swir1", "swir2", "pan", "cir", "tirs1", "tirs2", "ndvi"]]
+X_test_no_last_crop = X_test[["month", "b", "g",
+           "r", "nir", "swir1", "swir2", "pan", "cir", "tirs1", "tirs2", "ndvi"]]
 rand_for_no_last_crop.fit(X_train_no_last_crop, y_train)
 y_pred_no_last = rand_for_no_last_crop.predict(X_test_no_last_crop)
 accuracy_no_last = accuracy_score(y_test, y_pred_no_last)
