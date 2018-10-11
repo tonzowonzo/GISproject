df66e6031e2d278947f31f48c4de2f8633c3fcde
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-10-11 22:56:23 +0200

    Add files via upload

--- temp_from_metadata.py
+++ temp_from_metadata.py
@@ -2,9 +2,10 @@
 import os
 import re
 import math
+import numpy as np
 
 # File path to open in eg.
-path = r"C:/Users/Tim/Desktop/GIS/GISproject/landsat/Bulk Order 948741/Landsat 4-5 TM C1 Level-1/LT05_L1GS_195026_20100613_20161015_01_T2_MTL.txt"
+path = r"C:/Users/Tim/Desktop/GIS/GISproject/landsat/metadata/LC08_L1TP_195026_20140811_20170420_01_T1_MTL.txt"
 num_regex = re.compile(r"\d+\.\d+")
 
 def get_required_info_from_metadata(path, landsat):
@@ -49,7 +50,7 @@ def get_required_info_from_metadata(path, landsat):
             
     return mult, add, k1, k2
 
-mult, add, k1, k2 = get_required_info_from_metadata(path, 7)
+#get_required_info_from_metadata(path, 8)
 
 def calculate_lst(ndvi, ndvi_max, ndvi_min, thermal_band, landsat, mult, add, k1, k2):
     '''
@@ -65,25 +66,16 @@ def calculate_lst(ndvi, ndvi_max, ndvi_min, thermal_band, landsat, mult, add, k1
         k1: K1_CONSTANT_BAND_6 or 10 from metadata
         k2: K2_CONSTANT_BAND_6 or 10 from metadata
     '''
-    mult = mult / 10
-    print(mult)
     # Calculate top of atmosphere temperature.
     top_of_atmosphere = mult * thermal_band + add
-    print(top_of_atmosphere)
     # Calculate the temperature caused by brightness.
-    brightness_temperature = (k2/(math.log(k1/top_of_atmosphere) + 1)) - 273.15
-    print(brightness_temperature)
+    brightness_temperature = (k2/(np.log(k1/top_of_atmosphere) + 1)) - 273.15
     # Calculate proportion of vegetation.
     Pv = ((ndvi - ndvi_min)/(ndvi_max - ndvi_min))**2
-    print(Pv)
     # Calculate emissivity.
     emissivity = 0.004 * Pv + 0.986
-    print(emissivity)
     # Calculate land surface temperature.
-    LST = (brightness_temperature/(1 + (0.00115*brightness_temperature/1.4388) * math.log(emissivity)))
-    
+    at_sat_temp = (brightness_temperature/(1 + (0.00115*brightness_temperature/1.4388) * np.log(emissivity)))
+    LST = (at_sat_temp / 1 + 11.5 * (at_sat_temp / 14380) * np.log(emissivity))
+    LST = LST / 50
     return LST
-
-LST = calculate_lst(0.6, 0.9, 0.3, 54, 8, mult, add, k1, k2)
-    
-print(LST)
