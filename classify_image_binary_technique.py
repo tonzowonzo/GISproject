aec414f66b20ce53d8b848df85b6d7c251d55fcc
Tim <35193581+tonzowonzo@users.noreply.github.com>
2019-01-08 23:25:47 +0100

    Add files via upload

--- classify_image_binary_technique.py
+++ classify_image_binary_technique.py
@@ -15,7 +15,7 @@ model_2a = joblib.load("for_clf_2a.pkl")
 model_3c = joblib.load("for_clf_3c.pkl")
 model_4a = joblib.load("for_clf_4a.pkl")
 path = r"C:/Users/Tim/Desktop/GIS/GISproject/ammertal_clf_area/"
-march = "20140329.tif"
+march = "20110321.tif"
 april = "20110422.tif"
 may = "20110508.tif"
 july = "20110719.tif"
@@ -35,6 +35,7 @@ def crop_classification(march, may, july, april):
     image_date = march.split(".")[0]
     image_date = pd.to_datetime(image_date, format="%Y%m%d")
     month = image_date.month
+    year = image_date.year
     day_of_year = image_date.timetuple().tm_yday
     
     # Step one - classify summer vs winter crop.
@@ -49,7 +50,7 @@ def crop_classification(march, may, july, april):
     ndvi_march = numer / denom
     ndvi_ratio_march = ndvi_march * month
     mask = np.where(nir_march == 0, 1, 0)
-    data = {"day_of_year": day_of_year,
+    data = {"day_of_year": day_of_year, "month": month, "year": year,
             "b": b_march, "g": g_march, "r": r_march, "nir": nir_march,
             "swir1": swir1_march, "tirs1": tirs1_march, "ndvi": ndvi_march,
             "ndvi_ratio": ndvi_ratio_march, "mask": mask}
@@ -65,6 +66,48 @@ def crop_classification(march, may, july, april):
             dataframe_full.iloc[:, :-1])
     probas = binary_classifier.predict_proba(
             dataframe_full.iloc[:, :-2])
+    
+    # Classify winter rape vs winter wheat.
+    # Remake dataframe.
+    image_date = april.split(".")[0]
+    image_date = pd.to_datetime(image_date, format="%Y%m%d")
+    month = image_date.month
+    year = image_date.year
+    day_of_year = image_date.timetuple().tm_yday
+    b_april = cv2.imread(path + "B/" + april, 0)
+    b_april = cv2.resize(b_april, (808, 942)).ravel()
+    g_april = cv2.imread(path + "G/" + april, 0).ravel()
+    g_april = cv2.resize(g_april, (808, 942)).ravel()
+    r_april = cv2.imread(path + "R/" + april, 0).ravel()
+    r_april = cv2.resize(r_april, (808, 942)).ravel()
+    nir_april = cv2.imread(path + "NIR/" + april, 0).ravel()
+    nir_april = cv2.resize(nir_april, (808, 942)).ravel()
+    swir1_april = cv2.imread(path + "swir1/" + april, 0).ravel()
+    swir1_april = cv2.resize(swir1_april, (808, 942)).ravel()
+    tirs1_april = cv2.imread(path + "tirs1/" + april, 0).ravel()
+    tirs1_april = cv2.resize(tirs1_april, (808, 942)).ravel()
+    numer = nir_april - r_april
+    denom = nir_april + r_april
+    ndvi_april = numer / denom
+    ndvi_ratio_april = ndvi_april * month
+    data = {"day_of_year": day_of_year, "month": month, "year": year,
+            "b": b_april, "g": g_april, "r": r_april, "nir": nir_april,
+            "swir1": swir1_april, "tirs1": tirs1_april, "ndvi": ndvi_april,
+            "ndvi_ratio": ndvi_ratio_april, "mask": mask}
+    dataframe_2a = pd.DataFrame(data=data)
+    dataframe_2a = dataframe_2a.replace([np.inf, -np.inf], np.nan)
+    dataframe_2a = dataframe_2a.fillna(-1)
+    dataframe_2a["2a"] = -1
+    preds_2a = model_2a.predict(dataframe_2a.iloc[:, :-2])
+    print(preds_2a.max())
+    dataframe_full["2a"] = np.where(dataframe_full["1"] == 0,
+                  -1, preds_2a)
+    
+    # Sugarbeet from corn.
+#    dataframe_full["3c"] = -1
+#    preds_3c = model_3c.predict(dataframe_full.iloc[:, :-4])
+#    dataframe_full["3c"] = np.where(dataframe_full["1"] == 1,
+#                  -1, preds_3c)
     return dataframe_full, probas
 
     # Step two - classify rape vs barley where the crop is winter.
@@ -77,7 +120,7 @@ df, probas = crop_classification(march, may, july, april)
 def plot_output(df, probas, initial_shape=(808, 942)):
     
     preds = df.iloc[:, -1]
-    preds[df["mask"] == 1] = 2
+    preds[df["mask"] == 1] = -1
     preds = preds.values
     preds = preds.reshape(initial_shape)
     cv2.imwrite("test_ammertal.png", preds)
@@ -87,7 +130,7 @@ def plot_output(df, probas, initial_shape=(808, 942)):
     
     # Plot probabilities.
     for i in range(probas.shape[1]):
-        probas[df["mask"] == 1] = 0
+        probas[df["mask"] == 1] = -1
         probs = probas[:, i].reshape(initial_shape)
         plt.imshow(probs)
         plt.colorbar()
